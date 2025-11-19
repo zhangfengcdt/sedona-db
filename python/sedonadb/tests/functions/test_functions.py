52913bdcc82a7776accf0d1cb7df5e2a29209b87
Abeeujah <100226788+Abeeujah@users.noreply.github.com>
2025-11-19 16:34:51 +0100

    feat(c/sedona-geos): Implement ST_MinimumClearance (#314)
    Co-authored-by: Dewey Dunnington <dewey@dunnington.ca>

--- python/sedonadb/tests/functions/test_functions.py
+++ python/sedonadb/tests/functions/test_functions.py
@@ -2178,6 +2178,81 @@ def test_st_makevalid(eng, geom, expected):
 @pytest.mark.parametrize(
     ("geom", "expected"),
     [
+        (None, None),
+        (
+            "POINT (1 1)",
+            float("inf"),
+        ),
+        (
+            "LINESTRING (0 0, 1 1, 2 2)",
+            1.4142135623730951,
+        ),
+        (
+            "LINESTRING (5 107, 54 84, 101 100)",
+            49.64876634922564,
+        ),
+        (
+            "LINESTRING(0 0,0 0,1 1,1 1,2 2)",
+            1.4142135623730951,
+        ),
+        (
+            "POLYGON((0 0,0 3,3 3,3 0,0 0),(1 1,1 2,2 2,2 1,1 1))",
+            1.0,
+        ),
+        (
+            "POLYGON((0 0,0 1,0 1,1 1,1 0,0 0,0 0))",
+            1.0,
+        ),
+        (
+            "POLYGON EMPTY",
+            float("inf"),
+        ),
+        (
+            "POLYGON((0 0,3 0,3 3,2 1,1 3,0 3,0 0))",
+            1.0,
+        ),
+        (
+            "MULTIPOINT (10 40, 40 30)",
+            31.622776601683793,
+        ),
+        (
+            "MULTIPOINT ((10 10), (20 20), (30 30))",
+            14.142135623730951,
+        ),
+        (
+            "MULTILINESTRING ((10 10, 20 20), (30 30, 40 40))",
+            14.142135623730951,
+        ),
+        (
+            "MULTILINESTRING ((5 5, 10 5, 10 15), (20 20, 25 10))",
+            5.0,
+        ),
+        (
+            "MULTIPOLYGON(((26 125, 26 200, 126 200, 126 125, 26 125 ),( 51 150, 101 150, 76 175, 51 150 )),(( 151 100, 151 200, 176 175, 151 100 )))",
+            25.0,
+        ),
+        (
+            "MULTIPOLYGON(((0.5 0.5,0 0,0 1,0.5 0.5)),((0.5 0.5,1 1,1 0,0.5 0.5)),((2.5 2.5,2 2,2 3,2.5 2.5)),((2.5 2.5,3 3,3 2,2.5 2.5)))",
+            0.5,
+        ),
+        (
+            "GEOMETRYCOLLECTION(POINT(1 1),MULTIPOLYGON(((0 2,1 1,0 0,0 2)),((2 0,1 1,2 2,2 0))))",
+            1.0,
+        ),
+    ],
+)
+def test_st_minimum_clearance(eng, geom, expected):
+    eng = eng.create_or_skip()
+    eng.assert_query_result(
+        f"SELECT ST_MinimumClearance({geom_or_null(geom)})",
+        expected,
+    )
+
+
+@pytest.mark.parametrize("eng", [SedonaDB, PostGIS])
+@pytest.mark.parametrize(
+    ("geom", "expected"),
+    [
         (
             None,
             None,
