326f26aeb8bb69533616feea7505ee58f26db90f
Ubuntu <ubuntu@ip-172-31-3-182.us-west-2.compute.internal>
2025-12-17 04:39:28 +0000

    delete unused files

--- c/sedona-libgpuspatial/libgpuspatial/benchmark/common.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export BENCHMARK_ROOT="/home/ubuntu/.cache/clion/gpuspatial-main/build"
-export S3_POLYGON_DATASETS=("wherobots-benchmark-prod/data/3rdparty-bench/postal-codes-sorted" "wherobots-benchmark-prod/data/sedona-db/california/BE/overture-buildings")
-export S3_POINT_DATASETS=("wherobots-benchmark-prod/data/sedona-db/california/BE/nodes" "wherobots-benchmark-prod/data/3rdparty-bench/osm-nodes-large-sorted-corrected")
-export POINT_FILES_LIMIT=10
-export DEFAULT_BATCH_SIZE=262144
