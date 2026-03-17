7a71e52815ceeb2b6dc5312af83303cde8fa8d71
Liang Geng <geng.161@osu.edu>
2026-03-17 23:51:30 +0800

    feat(c/libgpuspatial): Upgrade RAPIDS RMM to 25.12 (#718)

--- c/sedona-libgpuspatial/libgpuspatial/cmake/thirdparty/get_rmm.cmake
+++ c/sedona-libgpuspatial/libgpuspatial/cmake/thirdparty/get_rmm.cmake
@@ -37,6 +37,35 @@ function(find_and_configure_rmm)
   rapids_cpm_rmm(BUILD_EXPORT_SET gpuspatial-exports INSTALL_EXPORT_SET
                  gpuspatial-exports)
 
+  # --- INJECT RMM HOTFIX ---
+  # We only want to apply the patch if CPM actually fetched RMM from source.
+  # If rmm_SOURCE_DIR is defined, it means it was downloaded into the build tree.
+  message("rmm_SOURCE_DIR: ${rmm_SOURCE_DIR}")
+  if(DEFINED rmm_SOURCE_DIR)
+    set(RMM_PATCH_TARGET_DIR "${rmm_SOURCE_DIR}/cpp/include/rmm/mr")
+    set(RMM_PATCH_MARKER "${rmm_SOURCE_DIR}/.rmm_race_condition_patched")
+
+    # If the target directory exists and hasn't been patched yet
+    if(EXISTS "${RMM_PATCH_TARGET_DIR}" AND NOT EXISTS "${RMM_PATCH_MARKER}")
+      message(STATUS "RAPIDS-CMAKE BYPASS: Applying custom race-condition patch to RMM..."
+      )
+
+      execute_process(# Assuming the patch file is located in the root 'cmake/patches/' directory
+                      COMMAND patch -p0 -i
+                              "${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/rmm_race_condition.patch"
+                      WORKING_DIRECTORY "${RMM_PATCH_TARGET_DIR}"
+                      RESULT_VARIABLE PATCH_RESULT)
+
+      if(PATCH_RESULT EQUAL 0)
+        file(TOUCH "${RMM_PATCH_MARKER}")
+        message(STATUS "Successfully patched RMM tracking adaptor.")
+      else()
+        message(FATAL_ERROR "Failed to apply RMM patch. Check the patch file formatting.")
+      endif()
+    endif()
+  endif()
+  # -------------------------
+
 endfunction()
 
 find_and_configure_rmm()
